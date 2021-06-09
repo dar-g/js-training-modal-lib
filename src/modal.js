414bf87cc3ae26d05e9484f5390fb040703d9626
Daria Goriana <goriana.d@gmail.com>
2021-06-09 18:30:33 +0300

    Working click to open and hide modal

--- src/modal.js
+++ src/modal.js
@@ -2,25 +2,44 @@ class MyModal {
     constructor(modalObj) {
         this.modalTitle = modalObj.title;
         this.modalMessage = modalObj.message;
-        this.modalWrapper = document.createElement('div');
-        this.text = document.createTextNode(this.modalTitle);
+        // this.onConfirm = modalObj.onConfirm;
     }
 
-    create() {
-        document.body.append(this.modalWrapper);
-        this.modalWrapper.appendChild(this.text);
-        this.modalWrapper.classList.add('modal-wrapper');
+    renderModal() {
+        this.myModal = document.createElement('div');
+        this.myModal.classList.add('my-modal');
+
+        const modalTemplate = `
+            <div class="modal">
+                <div class="title-wrapper">${this.modalTitle}</div>
+                <div class="message-wrapper">${this.modalMessage}</div>
+                <div class="btn-wrapper">
+                    <button class="ok-btn" onclick="(() => document.body.style.backgroundColor = 'yellow')()">Ok</button>
+                    <button class="cancel-btn">x</button>
+                </div>
+            </div>
+        `;
+
+        this.myModal.insertAdjacentHTML('afterbegin', modalTemplate);
+
+        document.body.append(this.myModal);
+
+        const okBtn = document.body.getElementsByClassName('ok-btn')[0];
+        // okBtn.addEventListener('click', () => {
+        //     this.onConfirm();
+        // });
+
+        // const cancelBtn = document.body.getElementsByClassName('cancel-btn')[0];
+        // cancelBtn.addEventListener('click', );
     }
 
     showModal() {
-        this.modalWrapper.classList.add('show');
+        this.myModal.classList.add('show');
     }
 
     hideModal() {
-        this.modalWrapper.classList.remove('show');
+        this.myModal.classList.remove('show');
     }
-
 }
-
 
 export default MyModal;
